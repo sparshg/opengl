d1d8ccaf0bb723b445bb86a7736768b4928eafa8
sparshg <43041139+sparshg@users.noreply.github.com>
2024-03-23 15:07:21 +0530

    index buffers

--- resources/vertex.vert
+++ resources/vertex.vert
@@ -1,10 +1,11 @@
 #version 330 core 
 
-layout(location = 0) in vec3 position;
-out vec3 fragPos;
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec3 color;
+out vec3 fColor;
 
 void main()
 {
-	fragPos = position;
-	gl_Position = vec4(position.xyz, 1.0f);
+	fColor = color;
+	gl_Position = position;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,54 +8,37 @@
 int main() {
     GLFWwindow* window = initialize_window(4, 1, true, true);
     clearErrors();
-
+    // clang-format off
     float vertices[] = {
-        1.0f,
-        -1.0f,
-        1.0f,
-        0.0f,
-        0.0f,
-        -1.0f,
-        -1.0f,
-        0.0f,
-        1.0f,
-        0.0f,
-        1.0f,
-        1.0f,
-        0.0f,
-        0.0f,
-        1.0f,
-        1.0f,
-        1.0f,
-        0.0f,
-        0.0f,
-        0.0f,
-        -1.0f,
-        -1.0f,
-        0.0f,
-        0.0f,
-        0.0f,
-        -1.0f,
-        1.0f,
-        0.0f,
-        0.0f,
-        0.0f,
-
+        // x, y, z, r, g, b
+        1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
     };
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 1, 3,
+    };
+    // clang-format on
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
     GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     // glBindVertexArray(0);
 
     Shader s;
@@ -63,7 +46,7 @@ int main() {
     s.bind();
 
     GLint timeLocation = s.getUniform("u_Time");
-    glUniform1f(timeLocation, 0.0f);
+    glUniform1f(timeLocation, 0.4f);
     double offset = glfwGetTime();
 
     getErrors();
@@ -72,10 +55,10 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUniform1f(timeLocation, glfwGetTime() - offset);
+        glUniform1f(timeLocation, glfwGetTime() - offset + 0.4f);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
