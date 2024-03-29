f667d92c3ee84154192884a10b928779c92a984d
sparshg <43041139+sparshg@users.noreply.github.com>
2024-03-29 13:40:28 +0530

    add colors

--- resources/vertex.vert
+++ resources/vertex.vert
@@ -4,7 +4,5 @@ attribute vec3 p;
 
 void main()
 {
-	
 	gl_Position = vec4(p, 1);
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,11 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
+        // glColor3f(1, 0, 0);
         glVertex2f(0, 1);
+        // glColor3f(0, 1, 0);
         glVertex2f(1, -1);
+        // glColor3f(0, 0, 1);
         glVertex2f(-1, -1);
         glEnd();
 
